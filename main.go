2c28e1ece999f5e498ee1e2bc6a569c76ad3bfa4
Tatsuya <tatsu.mase@icloud.com>
2024-05-20 15:13:11 +0900

    トランザクションを利用した更新処理を実行してみよう

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"database/sql"
-	"dbsample/models"
+	// "dbsample/models"
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -20,23 +20,45 @@ func main() {
 	}
 	defer db.Close()
 
-	article := models.Article{
-		Title:	"insert test",
-		Contents:	"Can I insert data correctly?",
-		UserName:	"saki",
-	}
-
-	const sqlStr = `
-		insert into articles (title, contents, username, nice, created_at) values (?, ?, ?, 0, now())
-	`
-	
-	result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
+	tx, err := db.Begin()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	
-	fmt.Println(result.LastInsertId())
-	fmt.Println(result.RowsAffected())
+
+	// 現在の良いね数を取得するクエリの実行
+	article_id := 1
+	const sqlGetNice = `
+		select nice from articles where article_id = ?
+	`
+
+	row := tx.QueryRow(sqlGetNice, article_id)
+	if err != nil {
+		fmt.Println(err)
+		tx.Rollback()
+		return
+	}
+
+	// 変数nicenumに現在の良いね数を読み込む
+	var nicenum int
+	err = row.Scan(&nicenum)
+	if err != nil {
+		fmt.Println(err)
+		tx.Rollback()
+		return
+	}
+
+	// 良いね数を1増やす
+	const sqlUpdateNice = `
+		update articles set nice = ? where article_id = ?
+	`
+	_, err = tx.Exec(sqlUpdateNice, nicenum+1, article_id)
+	if err != nil {
+		fmt.Println(err)
+		tx.Rollback()
+		return
+	}
+
+	tx.Commit()
 
 }
